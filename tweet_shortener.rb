28814373b51bcc2dd266f5867b9aa20c0b8cf8ea
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-06 02:08:44 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,16 @@
-
+def dictionary
+dictionary = {
+  "too" => "2",
+  "to" => "2",
+  "two" =>"2",
+  "four" => "4",
+  "for" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
+end
 
 # Write your code here.
 def word_substituter(string) 
