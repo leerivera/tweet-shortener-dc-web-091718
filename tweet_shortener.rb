3538e3ea18744e8e38f18b251ab82f57863f3d46
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-06 02:06:34 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def word_substituter(string) 
+  string.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else 
+      word 
+    end 
+  end.join(" ")
+end 
+word_substituter(tweet_one)
